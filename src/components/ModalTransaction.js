65def145ba50c85b69b940d2dcab6d9f6aed3328
irchamhabibie <irchamhabibie@gmail.com>
2021-06-20 19:44:50 +0700

    Deploy

---
+++ src/components/ModalTransaction.js
@@ -0,0 +1,29 @@
+import { useContext } from "react"
+import { Modal, Image } from "react-bootstrap"
+import { UserContext } from "../contexts/userContext"
+
+function ModalTransaction() {
+  const [state, dispatch] = useContext(UserContext)
+  const handleModalTransaction = () => {
+    dispatch({
+      type: "TRANSACTIONMODALTUTUP",
+    })
+  }
+  return (
+    <>
+      <Modal
+        size='sm'
+        centered
+        show={state.isVisibleTransaction}
+        onHide={() => handleModalTransaction()}>
+        <Image
+          style={{ width: "100%", height: "100%" }}
+          src={
+            "http://localhost:5000/public/uploads/" + state.buktiTransfer
+          }></Image>
+      </Modal>
+    </>
+  )
+}
+
+export default ModalTransaction
